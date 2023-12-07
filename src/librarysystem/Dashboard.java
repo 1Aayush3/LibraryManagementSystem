c7ef5fd8cf143f8ad2aaec4b803db9e3f34a381b
Sudin <sudean.mdrr@gmail.com>
2023-12-07 14:29:36 -0600

    dashboard

---
+++ src/librarysystem/Dashboard.java
@@ -0,0 +1,20 @@
+package librarysystem;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Dashboard extends JFrame {
+
+    Dashboard(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        this.setSize(Toolkit.getDefaultToolkit().getScreenSize());
+        this.setVisible(true);
+
+        
+    }
+
+
+
+
+}
